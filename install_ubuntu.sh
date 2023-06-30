5dfff13ac86e5fb8eade9dd85dc1f2212f4e61fc
hyperion-voting <132654932+hyperion-voting@users.noreply.github.com>
2023-06-30 06:34:49 +0200

    Updated install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y python3 python3-pip python3-wheel python3-gmpy2
 mkdir requirements && cd requirements
 git clone https://github.com/tompetersen/threshold-crypto.git
 cd threshold-crypto/
-git checkout 2870e48cefbe1f9af1aaccf18346d984a5a8a4a1
+python3 -m pip install -r requirements.txt
 pip install .
 cd ../../
 python3 -m pip install -r requirements.txt
